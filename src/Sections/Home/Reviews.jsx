c6bc11db361be3d017066a62d1ea96626e4d7f59
yasin2514 <yasinkhanrabbi1999@gmail.com>
2025-05-18 08:50:08 +0600

    reviews section updated

--- src/Sections/Home/Reviews.jsx
+++ src/Sections/Home/Reviews.jsx
@@ -73,8 +73,37 @@ const settings = {
   slidesToShow: 3,
   slidesToScroll: 1,
   responsive: [
-    { breakpoint: 1024, settings: { slidesToShow: 2 } },
-    { breakpoint: 768, settings: { slidesToShow: 1 } },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        vertical: false,
+        arrows: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        vertical: true,
+        verticalSwiping: true,
+        arrows: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        vertical: true,
+        verticalSwiping: true,
+        arrows: true,
+        dots: false,
+      },
+    },
   ],
 };
 
